ab83ed352bd27f75c132b03ddf2e0001efe2ec51
evan777git <evankusuma777@gmail.com>
2026-03-21 18:27:06 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_00000119455_YustinusEvanKusuma/week01/GameStore.kt
+++ src/oop_00000119455_YustinusEvanKusuma/week01/GameStore.kt
@@ -3,7 +3,20 @@ package oop_00000119455_YustinusEvanKusuma.week01
 fun main () {
     val gameTitle = "Game apalah"
     val price = 250_000
+
+    val finalPrice = calculateDiscount(price)
+
+    printReceipt(title = gameTitle, finalPrice = finalPrice)
 }
 
-fun calculateDiscount(price: Int) =
-    if(price > 500_000) price * 0.2 else price * 0.1
+// Tipe data harus konsisten. Operasi price * 0.8 menghasilkan Double karena melibatkan bilangan desimal, sedangkan fungsi diharapkan mengembalikan Int. Oleh karena itu, digunakan .toInt()
+fun calculateDiscount(price: Int) = if(price > 500_000)
+        (price * 0.8).toInt() else (price * 0.9).toInt()
+
+fun printReceipt(
+    title: String,
+    finalPrice: Int
+) {
+    println("Judul Game: $title")
+    println("Harga Akhir: Rp $finalPrice")
+}
