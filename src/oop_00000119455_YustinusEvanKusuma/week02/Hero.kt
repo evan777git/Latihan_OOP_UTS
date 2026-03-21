3357cde5251c26a9cedeef300f4d1f6b887fd052
evan777git <evankusuma777@gmail.com>
2026-03-21 22:29:24 +0700

    week02: (task 2) create Hero class

---
+++ src/oop_00000119455_YustinusEvanKusuma/week02/Hero.kt
@@ -0,0 +1,23 @@
+package oop_00000119455_YustinusEvanKusuma.week02
+
+class Hero (
+    val name: String,
+    val hp: Int = 100,
+    val baseDamage: Int
+) {
+    fun attack(targetName: String) {
+        println("$name menyerang $targetName dengan damage $baseDamage!")
+    }
+
+    fun takeDamage(damage: Int) {
+        hp -= damage
+        if (hp < 0) {
+            hp = 0
+        }
+        println("$name menerima $damage. Sisa HP: $hp")
+    }
+
+    fun isAlive(): Boolean {
+        return hp > 0
+    }
+}
