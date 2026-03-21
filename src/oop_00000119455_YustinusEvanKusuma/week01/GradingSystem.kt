0c37cb5773c539b623c8582f97d79cd58f1d0f71
evan777git <evankusuma777@gmail.com>
2026-03-21 16:36:27 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000119455_YustinusEvanKusuma/week01/GradingSystem.kt
@@ -0,0 +1,8 @@
+package oop_00000119455_YustinusEvanKusuma.week01
+
+fun main () {
+    var name: String = "John Thor"
+    var score: Int = 80
+
+    println("Nama:" + name + ", Nilai:" + score)
+}
