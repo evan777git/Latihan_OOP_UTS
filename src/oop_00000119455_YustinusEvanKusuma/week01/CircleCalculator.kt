c4a9219bcc80e868df902dd722393d24c76f02f7
evan777git <evankusuma777@gmail.com>
2026-03-21 17:38:14 +0700

    week01: (tugas 1) clean up unused argos and comments

--- src/oop_00000119455_YustinusEvanKusuma/week01/CircleCalculator.kt
+++ src/oop_00000119455_YustinusEvanKusuma/week01/CircleCalculator.kt
@@ -1,6 +1,6 @@
 package oop_00000119455_YustinusEvanKusuma.week01
 
-fun main (args: Array<String>) {
+fun main () {
     val radius = 7.0
     val pi = 3.14
 
@@ -9,13 +9,10 @@ fun main (args: Array<String>) {
     println("Radius: $radius, Area: $area")
 
     checkSize(area)
+
+    println(checkSize(area))
 }
 
 // Kotlin Tidak bisa menebak tipe parameter function, karena function bisa dipanggil dari mana aja (harus jelas tipenya)
-fun checkSize(area: Double) {
-    if (area > 100) {
-        println("This is a Big Circle")
-    } else {
-        println("This is a Small Circle")
-    }
-}
+fun checkSize(area: Double) =
+    if (area > 100) "This is a Big Circle" else "This is a Small Circle"
