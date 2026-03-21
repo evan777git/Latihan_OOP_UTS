fa33d8876912adb3cee2294701496309a6567a41
evan777git <evankusuma777@gmail.com>
2026-03-21 18:34:49 +0700

    week01: (tugas 2) implement null safety for user note

--- src/oop_00000119455_YustinusEvanKusuma/week01/GameStore.kt
+++ src/oop_00000119455_YustinusEvanKusuma/week01/GameStore.kt
@@ -3,10 +3,11 @@ package oop_00000119455_YustinusEvanKusuma.week01
 fun main () {
     val gameTitle = "Game apalah"
     val price = 250_000
+    val note: String? = null // Isi kalau ada catatan
 
     val finalPrice = calculateDiscount(price)
 
-    printReceipt(title = gameTitle, finalPrice = finalPrice)
+    printReceipt(title = gameTitle, finalPrice = finalPrice, note = note)
 }
 
 // Tipe data harus konsisten. Operasi price * 0.8 menghasilkan Double karena melibatkan bilangan desimal, sedangkan fungsi diharapkan mengembalikan Int. Oleh karena itu, digunakan .toInt()
@@ -15,8 +16,11 @@ fun calculateDiscount(price: Int) = if(price > 500_000)
 
 fun printReceipt(
     title: String,
-    finalPrice: Int
+    finalPrice: Int,
+    note: String?
 ) {
     println("Judul Game: $title")
     println("Harga Akhir: Rp $finalPrice")
+
+    println("Catatan: ${note?: "Tidak ada catatan"}")
 }
