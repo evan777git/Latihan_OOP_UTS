4738bf928132456ac1bc0b635883343fcf6b71a1
evan777git <evankusuma777@gmail.com>
2026-03-22 15:28:11 +0700

    week03: fix setter using backing field

--- src/oop_00000119455_YustinusEvanKusuma/week03/Main.kt
+++ src/oop_00000119455_YustinusEvanKusuma/week03/Main.kt
@@ -3,5 +3,5 @@ package oop_00000119455_YustinusEvanKusuma.week03
 fun main () {
     val e = Employee ("Budi")
     println("--- Test Error ---")
-    e.salary = 5000 // Bakal ledakin program karna setter dipanggil. Di dalam setter, baris "this.salary = value" memanggil setter lagi. Setter memanggil setter lagi. Terus menerus tanpa henti sampai memori stack habis.
+    e.salary = 5000
 }
