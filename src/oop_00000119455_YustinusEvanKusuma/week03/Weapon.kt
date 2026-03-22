91cc16310462c57c922cfe64177bed264ffd0066
evan777git <evankusuma777@gmail.com>
2026-03-22 16:30:58 +0700

    week03: (task 1) create Weapon class with validation

---
+++ src/oop_00000119455_YustinusEvanKusuma/week03/Weapon.kt
@@ -0,0 +1,28 @@
+package oop_00000119455_YustinusEvanKusuma.week03
+
+class Weapon(
+    val name: String
+    damage: Int
+) {
+    var damage: Int = damage
+
+        set(value) {
+            if(value < 0) {
+                println("Damage tidak boleh negatif. Otomatis ubah ke 0.")
+                return
+            } else if(value > 1000) {
+                println("Terlalu overpower! Otomatis ubah ke 1000.")
+                field = 1000
+                return
+            } else {
+                field = value
+            }
+        }
+
+    val tier: String
+        get() = when{
+            damage > 800 -> "Legendary"
+            damage > 500 -> "Epic"
+            else -> "Common"
+        }
+}
