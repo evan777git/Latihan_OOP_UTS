603d1e353b86ad3ae2054fcdda54ec4e0ce11b64
evan777git <evankusuma777@gmail.com>
2026-03-30 18:14:06 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000119455_YustinusEvanKusuma/week06/GadgetInterfaces.kt
@@ -0,0 +1,13 @@
+package oop_00000119455_YustinusEvanKusuma.week06
+
+interface Camera {
+    fun turnOn() {
+        println("Lensa kamera terbuka dan sensor aktif.")
+    }
+}
+
+interface Phone {
+    fun turnOn() {
+        println("Sinyal seluler mencari jaringan.")
+    }
+}
