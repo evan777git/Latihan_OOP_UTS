b8ec58e516215ec111472ea7aa2145b56ec75937
evan777git <evankusuma777@gmail.com>
2026-03-30 15:52:23 +0700

    week05: (task 1) test MathHelper overloading in main

---
+++ src/oop_00000119455_YustinusEvanKusuma/week05/MainMath.kt
@@ -0,0 +1,9 @@
+package oop_00000119455_YustinusEvanKusuma.week05
+
+fun main() {
+    val math = MathHelper()
+
+    println("Luas dari persegi adalah ${math.hitungLuas(7)}")
+    println("Luas dari persegi panjang adalah ${math.hitungLuas(7, 3)}")
+    println("Luas dari lingkaran adalah ${math.hitungLuas(3.5)}")
+}
