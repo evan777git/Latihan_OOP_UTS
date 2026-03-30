896579b66a96c926cc3552332eff299302b950ab
evan777git <evankusuma777@gmail.com>
2026-03-30 16:35:38 +0700

    week05: (task 2) add smart casting for EWallet topUp recovery

--- src/oop_00000119455_YustinusEvanKusuma/week05/MainWallet.kt
+++ src/oop_00000119455_YustinusEvanKusuma/week05/MainWallet.kt
@@ -1,5 +1,7 @@
 package oop_00000119455_YustinusEvanKusuma.week05
 
+import kotlin.system.exitProcess
+
 fun main() {
     val wallet = EWallet("Evan", 50_000.0)
     val creditCard = CreditCard("Evan777", 100_000.0)
@@ -8,5 +10,12 @@ fun main() {
 
     for(payment in payments) {
         payment.processPayment(75_000.0)
+
+        if (payment is EWallet) {
+            println("Saldo kurang, melakukan top up..")
+            payment.topUp(50_000.0)
+            payment.processPayment(75_000.0)
+        }
+        println()
     }
 }
