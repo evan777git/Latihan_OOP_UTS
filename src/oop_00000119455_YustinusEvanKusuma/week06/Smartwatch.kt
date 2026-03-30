834a0993bc83740c295f07ea07ec8858de3b5f8d
evan777git <evankusuma777@gmail.com>
2026-03-30 18:00:26 +0700

    week06: create Smartwatch with multiple implementations

---
+++ src/oop_00000119455_YustinusEvanKusuma/week06/Smartwatch.kt
@@ -0,0 +1,16 @@
+package oop_00000119455_YustinusEvanKusuma.week06
+
+// Gabungin 1 Parent Class dan 2 Interface
+class Smartwatch: Watch(), BluetoothConnectable, Rechargeable {
+    override fun showTime() {
+        println("Layar OLED menyala: 14:00 WIB")
+    }
+
+    override fun connectToBluetooth() {
+        println("Mencari perangkat HP di sekitar untuk pairing...")
+    }
+
+    override fun chargeBattery() {
+        println("Mengisi daya menggunakan charger magnetik 15W")
+    }
+}
