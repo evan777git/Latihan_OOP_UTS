1366409757bb19631033a415c941a29c44660ea9
evan777git <evankusuma777@gmail.com>
2026-03-30 21:52:07 +0700

    week06: (task) instantiate smart devices in main

--- src/oop_00000119455_YustinusEvanKusuma/week06/SmartCCTV.kt
+++ src/oop_00000119455_YustinusEvanKusuma/week06/SmartCCTV.kt
@@ -1,13 +1,13 @@
 package oop_00000119455_YustinusEvanKusuma.week06
 
 class SmartCCTV (override val id: String, override val name: String) : SmartDevice, Switchable, Recordable{
-    override fun starRecord() {
+    override fun startRecord() {
         println("CCTV $name mulai merekam.")
     }
 
     override fun turnOn() {
         println("CCTV $name dinyalan.")
-        starRecord()
+        startRecord()
     }
 
     override fun turnOff() {
