4617d7e5a60b018dbbe4bfcc6cfda2862961977c
evan777git <evankusuma777@gmail.com>
2026-03-31 15:47:50 +0700

    week07: (task) create ItemRarity enum

---
+++ src/oop_00000119455_YustinusEvanKusuma/week07/GameModels.kt
@@ -0,0 +1,9 @@
+package oop_00000119455_YustinusEvanKusuma.week07
+
+enum class ItemRarity(val dropChance: Int) {
+    COMMON(70),
+    UNCOMMON(30),
+    RARE(7),
+    EPIC(2),
+    LEGENDARY(1)
+}
