001c56c8ee6f1a07f4ec3baecc502584abba1f87
evan777git <evankusuma777@gmail.com>
2026-03-31 15:25:27 +0700

    week07: create ApiResponse sealed class

--- src/oop_00000119455_YustinusEvanKusuma/week07/SystemStates.kt
+++ src/oop_00000119455_YustinusEvanKusuma/week07/SystemStates.kt
@@ -4,3 +4,10 @@ package oop_00000119455_YustinusEvanKusuma.week07
 enum class AppState {
     STARTING, RUNNING, STOPPED
 }
+
+// sealed class adalah kelompok kemungkinan yang sudah ditentukan
+sealed class ApiResponse {
+    data class Success(val data: String) : ApiResponse()
+    data class Failure(val message: String) : ApiResponse()
+    object Loading : ApiResponse()
+} // ApiResponse cuma bisa jadi 3 hal: Success -> ada data, Failure -> ada pesan error, Loading -> sedang proses
