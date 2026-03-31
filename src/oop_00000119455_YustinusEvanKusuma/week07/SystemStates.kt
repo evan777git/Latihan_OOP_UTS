cc573ff4a1c6911968ae16fef678a4958383f2f4
evan777git <evankusuma777@gmail.com>
2026-03-31 15:19:47 +0700

    week07: create AppState enum

---
+++ src/oop_00000119455_YustinusEvanKusuma/week07/SystemStates.kt
@@ -0,0 +1,6 @@
+package oop_00000119455_YustinusEvanKusuma.week07
+
+// enum class adalah daftar pilihan yang sudah ditentukan dan terbatas (gak mungkin ada yang lain)
+enum class AppState {
+    STARTING, RUNNING, STOPPED
+}
