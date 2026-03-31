b0edc440d052b7b820b0bb51dd6c94980796ff70
evan777git <evankusuma777@gmail.com>
2026-03-31 14:20:07 +0700

    week07: (trial) trigger private constructor error on NetworkClient

---
+++ src/oop_00000119455_YustinusEvanKusuma/week07/Main.kt
@@ -0,0 +1,5 @@
+package oop_00000119455_YustinusEvanKusuma.week07
+
+fun main() {
+    val client = NetworkClient("hhtps://api.umn.ac.id")
+}
---
+++ src/oop_00000119455_YustinusEvanKusuma/week07/NetworkClient.kt
@@ -0,0 +1,7 @@
+package oop_00000119455_YustinusEvanKusuma.week07
+
+class NetworkClient(val url: String) {
+    fun connect() {
+        println("Connecting to $url...")
+    }
+}
